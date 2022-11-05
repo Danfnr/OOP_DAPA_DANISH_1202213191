a3f3f83eeadd8948e6146ed032c9d2602596fb91
Danfnr <116441092+Danfnr@users.noreply.github.com>
2022-11-05 14:09:59 +0700

    Add files via upload

---
+++ OOP_DAPA_DANISH_1202213191/Database.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+List<menu> Listmenu = new ArrayList<>()
+
+    // TODO Create Method to Insert Menu to Database
+    public void Insert Menu(Menu menu){
+        Listmenu.add(menu;)
+    }
+
+    // TODO Create Method to Show Menu from Database
+    public void showmenu(Menu menu){
+        System.out.println("Menu:");
+        System.out.println("'''''''''''''''''''''''''''");
+        for(int i = 0; <ListMenu.size())
+    }
+
+    // TODO Create Method to Search Menu from Database
+
+
+}
---
+++ OOP_DAPA_DANISH_1202213191/Menu.java
@@ -0,0 +1,19 @@
+public class Menu {
+
+    String name;
+    String category;
+    int price;
+
+    public Void setName(String name){
+        this.name = name;
+    }
+      
+    public Void setCategory(String category){
+        this.category = category;
+    }
+
+    public Void setPrice(Int price){
+        this.price = price;
+    }
+    
+}
